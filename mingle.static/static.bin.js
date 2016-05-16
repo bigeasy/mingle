8866b695d84b6c60c273d914baf06f59d184fb47
Alan Gutierrez <alan@prettyrobots.com>
2016-05-16 09:30:12 -0500

    Release Mingle Static 0.0.0.
    Closes #2.

--- mingle.static/static.bin.js
+++ mingle.static/static.bin.js
@@ -16,11 +16,14 @@
 
  */
 require('arguable')(module, require('cadence')(function (async, program) {
-    var prolific = require('prolific')
-    var logger = prolific.createLogger('bigeasy.mingle.static.bin')
-    var Shuttle = require('prolific.shuttle')
-    var Static = require('./http.js')
     var http = require('http')
+
+    var prolific = require('prolific')
+    var Shuttle = require('prolific.shuttle')
+
+    var Static = require('./http.js')
+
+    var logger = prolific.createLogger('bigeasy.mingle.static.bin')
 
     program.helpIf(program.command.param.help)
     program.command.required('bind')
