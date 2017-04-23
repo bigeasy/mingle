45566a1d82f8826fb5c7ea59defecb9f73308cd6
Alan Gutierrez <alan@prettyrobots.com>
2017-04-23 10:45:09 -0500

    Use Destructible.

--- mingle.static/static.bin.js
+++ mingle.static/static.bin.js
@@ -17,27 +17,33 @@
  */
 require('arguable')(module, require('cadence')(function (async, program) {
     var http = require('http')
-
-    var Shuttle = require('prolific.shuttle')
+    var destroyer = require('server-destroy')
 
     var Static = require('./middleware.js')
 
-    var logger = require('prolific.logger').createLogger('mingle.static')
+    var Shuttle = require('prolific.shuttle')
 
-    var shuttle = Shuttle.shuttle(program, logger)
+    var Destructible = require('destructible')
+    var Terminator = require('destructible/terminator')
 
     program.helpIf(program.ultimate.help)
     program.required('bind')
     program.validate(require('arguable/bindable'), 'bind')
 
+    var destructible = new Destructible('mingle.static')
+    destructible.events.pump(new Terminator(1000), 'push')
+    program.on('shutdown', destructible.destroy.bind(destructible))
+
+    var logger = require('prolific.logger').createLogger('mingle.static')
+    var shuttle = Shuttle.shuttle(program, logger)
+    destructible.addDestructor('shuttle', shuttle, 'close')
+
     var bind = program.ultimate.bind
 
-    var destroyer = require('server-destroy')
     var mingle = new Static(program.argv)
     var server = http.createServer(mingle.reactor.middleware)
     destroyer(server)
     server.listen(bind.port, bind.address, async())
-    program.on('shutdown', server.destroy.bind(server))
-    program.on('shutdown', shuttle.close.bind(shuttle))
+    destructible.addDestructor('http', server, 'destroy')
     logger.info('started', { parameters: program.ultimate })
 }))
