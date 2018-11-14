98c8795316cdd51ba5724c5a587ee63137803748
Alan Gutierrez <alan@prettyrobots.com>
2018-11-14 03:12:47 -0600

    Upgrade Olio.

--- mingle.executable/olio.js
+++ mingle.executable/olio.js
@@ -1,19 +1,22 @@
 var cadence = require('cadence')
 var Conduit = require('conduit/conduit')
 
+function Listener (resolver) {
+    this.resolver = resolver
+}
+
+Listener.prototype.connect = cadence(function (async, destructible, inbox, outbox) {
+    destructible.monitor('conduit', Conduit, inbox, outbox, this, cadence(function (async, request, inbox, outbox) {
+        this.resolver.resolve(async())
+    }), async())
+})
+
 module.exports = cadence(function (async, destructible, binder, properties) {
-    console.log('------', properties)
     var constructor = require(properties.module)
     delete properties.module
     async(function () {
         constructor(properties, async())
     }, function (resolver) {
-        binder.listen(cadence(function (async, destructible, inbox, outbox) {
-            destructible.monitor('conduit', Conduit, inbox, outbox, cadence(function (async, request, inbox, outbox) {
-                resolver.resolve(async())
-            }), async())
-        }), async())
-    }, function (olio) {
-        return [ binder.index ]
+        return new Listener(resolver)
     })
 })
--- mingle.executable/t/client.js
+++ mingle.executable/t/client.js
@@ -3,15 +3,11 @@ var http = require('http')
 var delta = require('delta')
 var Conduit = require('conduit/conduit')
 
-module.exports = cadence(function (async, destructible, binder) {
+module.exports = cadence(function (async, destructible, olio) {
     var logger = require('prolific.logger').createLogger('olio.http')
     var Reactor = require('reactor')
     var Caller = require('conduit/caller')
-    async(function () {
-        binder.listen(null, async())
-    }, function (olio) {
-        olio.sender('olio', cadence(function (async, destructible, inbox, outbox) {
-            destructible.monitor('conduit', Conduit, inbox, outbox, null, async())
-        }), async())
-    })
+    olio.sender('olio', cadence(function (async, destructible, inbox, outbox) {
+        destructible.monitor('conduit', Conduit, inbox, outbox, null, async())
+    }), async())
 })
