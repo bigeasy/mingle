2cad2eeea2398a9d8b3cbfa677056d4145b9d48a
Alan Gutierrez <alan@prettyrobots.com>
2018-11-20 06:23:04 -0600

    Convert to support Olio only.

--- mingle.static/static.argv.js
+++ mingle.static/static.argv.js
@@ -1,42 +1,19 @@
-/*
+var cadence = require('cadence')
+var coalesce = require('extant')
+var sprintf = require('sprintf-js').sprintf
 
-    ___ usage ___ en_US ___
-    mingle static [address:port, address:port...]
-
-    options:
-
-        -f, --format <format>
-
-          A sprintf format for address and port. A `sprintf` is invoked with
-          with the pattern and host name and port as first and second arguments
-          respectively. The pattern defaults to "%s:%d" to create a result like
-          "127.0.0.1:8080".
-
-        --help
-
-          Display help message.
-
-    ___ $ ___ en_US ___
-
-        bind is required:
-            the `--bind` argument is a required argument
-    ___ . ___
-
- */
-require('arguable')(module, require('cadence')(function (async, program) {
-    program.helpIf(program.ultimate.help)
-    program.validate(require('arguable/bindable'), 'bind')
-
-    var sprintf = require('sprintf-js').sprintf
-    var coalesce = require('extant')
-
-    var format = coalesce(program.ultimate.format, '%s:%d')
-    var addresses = program.argv.map(function (address) {
+function Resolver (properties) {
+    var format = coalesce(properties.format, '%s:%d')
+    this._addresses = properties.addresses.map(function (address) {
         var pair = address.split(':')
         return sprintf(format, pair[0], +pair[1])
     })
+}
 
-    return {
-        resolve: function (callback) { callback(null, addresses) }
-    }
-}))
+Resolver.prototype.resolve = cadence(function (async) {
+    return [ this._addresses ]
+})
+
+module.exports = cadence(function (async, destructible, properties) {
+    return new Resolver(properties)
+})
--- mingle.static/t/static.argv.t.js
+++ mingle.static/t/static.argv.t.js
@@ -1,12 +1,25 @@
-require('proof')(1, require('cadence')(prove))
+require('proof')(1, prove)
 
-function prove (async, okay) {
-    var argv = require('../static.argv')
-    async(function () {
-        argv([ '--format', 'http://%s:%d/', '127.0.0.1:8080' ], async())
-    }, function (resolver) {
-        resolver.resolve(async())
-    }, function (addresses) {
-        okay(addresses, [ 'http://127.0.0.1:8080/' ], 'resolve')
-    })
+function prove (okay, callback) {
+    var Destructbile = require('destructible')
+    var destructible = new Destructbile('t/static.t')
+
+    destructible.completed.wait(callback)
+
+    var Resolver = require('../static.argv')
+
+    var cadence = require('cadence')
+
+    cadence(function (async) {
+        async(function () {
+            destructible.monitor('resolver', Resolver, {
+                format: 'http://%s:%d/',
+                addresses: [ '127.0.0.1:8080' ]
+            }, async())
+        }, function (resolver) {
+            resolver.resolve(async())
+        }, function (addresses) {
+            okay(addresses, [ 'http://127.0.0.1:8080/' ], 'resolve')
+        })
+    })(destructible.monitor('test'))
 }
