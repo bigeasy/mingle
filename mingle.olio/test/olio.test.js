7a3a95c510201874607b82d63235801717f9c589
Alan Gutierrez <alan@prettyrobots.com>
2019-10-13 06:02:57 -0500

    Revert Mingle Olio tests to Proof.
    See #197.

--- mingle.olio/test/olio.test.js
+++ mingle.olio/test/olio.test.js
@@ -1,31 +1,28 @@
-describe('olio', () => {
-    const assert = require('assert')
-    it('can serve', async () => {
-        const fs = require('fs').promises
-        const once = require('prospective/once')
-        const path = require('path')
-        const children = require('child_process')
-        const axios = require('axios')
-        try {
-            await fs.unlink(path.join(__dirname, 'socket'))
-        } catch (error) {
-            if (error.code != 'ENOENT') {
-                throw error
-            }
+require('proof')(3, async (okay) => {
+    const fs = require('fs').promises
+    const once = require('prospective/once')
+    const path = require('path')
+    const children = require('child_process')
+    const axios = require('axios')
+    try {
+        await fs.unlink(path.join(__dirname, 'socket'))
+    } catch (error) {
+        if (error.code != 'ENOENT') {
+            throw error
         }
-        const olio = children.spawn(path.resolve(__dirname, '../node_modules/.bin/olio'), [
-            '--application', path.join(__dirname, 'application.js'),
-            '--configuration', path.join(__dirname, 'configuration.js'),
-        ], {
-            stdio: [ 'inherit', 'inherit', 'inherit', 'ipc' ]
-        })
-        const exit = once(olio, 'close')
-        const [ message ] = await once(olio, 'message')
-        assert.equal(message, 'olio:ready', 'ready')
-        const discover = await axios.get('http://127.0.0.1:8081/discover')
-        assert.deepStrictEqual(discover.data, [ '127.0.0.1:8888' ], 'discover')
-        olio.kill()
-        const [ code ] = await exit
-        assert.equal(code, 0, 'exit')
+    }
+    const olio = children.spawn(path.resolve(__dirname, '../node_modules/.bin/olio'), [
+        '--application', path.join(__dirname, 'application.js'),
+        '--configuration', path.join(__dirname, 'configuration.js'),
+    ], {
+        stdio: [ 'inherit', 'inherit', 'inherit', 'ipc' ]
     })
+    const exit = once(olio, 'close')
+    const [ message ] = await once(olio, 'message')
+    okay(message, 'olio:ready', 'ready')
+    const discover = await axios.get('http://127.0.0.1:8081/discover')
+    okay(discover.data, [ '127.0.0.1:8888' ], 'discover')
+    olio.kill()
+    const [ code ] = await exit
+    okay(code, 0, 'exit')
 })
