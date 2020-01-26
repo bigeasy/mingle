2d83e168089c5c6ae9639cbe8b4c3373f6490183
Alan Gutierrez <alan@prettyrobots.com>
2020-01-26 10:41:27 -0600

    Fix resolver constructor invocation.
    The resolvers are all expecting a Destructible as a first argument.
    Although none of them use it, it seems like it's worth preserving for
    the one that will use it someday. Possibly, UDP.

--- mingle.olio/olio.js
+++ mingle.olio/olio.js
@@ -1,5 +1,5 @@
 module.exports = async function (destructible, olio, properties) {
-    const resolver = await require(properties.module).create(properties)
+    const resolver = await require(properties.module).create(destructible, properties)
     return async function (request, inbox, outbox) {
         return await resolver.resolve()
     }
