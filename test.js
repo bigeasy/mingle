d9759e5e9745d973f2ce53356d6baa1ad0f5e796
Alan Gutierrez <alan@prettyrobots.com>
2021-02-04 18:04:28 -0600

    CI/CD debug.

--- test.js
+++ test.js
@@ -11,6 +11,7 @@ fs.readdirSync(__dirname)
     if (tests.filter(test => /\.t\.js$/.test(test)).length != 0) {
         const spawn = child.spawnSync('npm', [ 'test' ], { cwd: path.join(__dirname, dir), stdio: 'inherit' })
         if (spawn.status != 0) {
+            console.log('failed', path.join(__dirname, dir))
             process.exit(1)
         }
     }
